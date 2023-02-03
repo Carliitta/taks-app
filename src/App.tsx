f949a137048824053ee1ba96cc4c56a3fcb4f2b2
carliitta <carla.rodriguez340@gmail.com>
2023-02-03 13:51:23 -0300

    todolisto

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState ,useRef } from 'react';
+import './assets/tailwind.css';
 
-function App() {
+type formElement=React.FormEvent<HTMLFormElement>
+
+interface ITask{
+  name:string;
+  done:boolean;
+}
+function App() : JSX.Element{
+  const [newTask, setNewTask] = useState<string>(""); //nueva tarea
+  const [tasks, setTasks] = useState<ITask[]>([]);  //tareas
+  const taskInput = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: formElement): void => {
+    e.preventDefault();
+    addTask(newTask);
+    setNewTask("");
+    taskInput.current?.focus();
+  };
+
+  const addTask = (name: string): void => {
+    const newTasks: ITask[] = [...tasks, { name, done: false }];
+    setTasks(newTasks);
+  };
+
+  const toggleDoneTask = (i: number): void => {
+    const newTasks: ITask[] = [...tasks];
+    newTasks[i].done = !newTasks[i].done;
+    setTasks(newTasks);
+  };
+
+  const removeTask = (i: number): void => {
+    const newTasks: ITask[] = [...tasks];
+    newTasks.splice(i, 1);
+    setTasks(newTasks);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Fragment>
+    <div className="card">
+
+      <h1 className='title'>TO DO LIST</h1>
+    <form className='form' onSubmit={handleSubmit}  >
+      <input className="input" ref={taskInput}  required autoFocus
+       type="text" onChange={(e)=>setNewTask(e.target.value)} value={newTask} />
+      <button className="btn btn-blue btn-blue:hover">Add</button>
+    </form>
+    {
+      tasks.map((el,i:number)=>{
+        return(
+          <ul className='ul'>
+            <li style={{textDecoration: el.done?'line-through' :''}} className="" key={i}>{el.name}</li>
+            <button style={{background: !el.done?'' :'green'}}
+             onClick={() => toggleDoneTask(i)} className="btn-delete">
+            {el.done ? "✓" : "✗" }</button>
+            <button
+                      onClick={() => removeTask(i)}
+                      className="btn-basura"
+                    >
+                      🗑️
+                    </button>
+          </ul>
+          
+        )
+      })
+    }
+    
     </div>
+  </Fragment>
   );
 }
 
